5181cec9bc6a53bd484d6fdc82733fa77ec86b16
Joportus <81198811+Joportus@users.noreply.github.com>
2021-09-28 15:25:23 -0300

    modelo funcionando

--- programaConstituyentes.py
+++ programaConstituyentes.py
@@ -1,6 +1,9 @@
 
+from numpy import NAN, NaN, nan, string_
+import numpy
 import pandas as pd
 from openpyxl import load_workbook
+import math
 
 #Constituyente que patrocina
 iNombre = 0
@@ -18,29 +21,33 @@ iVicepresidencias = 9
 
 NombreReglamento = [iNombre, iReglamento]
 
+
 fnDatos = r'C:\Users\Joaquin\Desktop\Constituyente\trabajoconstituyente\Datos.xlsx'
 fnDatosPrueba = r'C:\Users\Joaquin\Desktop\Constituyente\trabajoconstituyente\DatosPrueba.xlsx'
 
 PatrocinadoresDeComisionReglamento = pd.read_excel(fnDatos, usecols=NombreReglamento)
 
-datoAEscribir = pd.DataFrame({'NOMBRE': ['Hola', 'Chao']})
+
+#writer = pd.ExcelWriter(fnDatosPrueba, engine='openpyxl')
+#book = load_workbook(fnDatosPrueba)
+#writer.book = book
+#writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
 
 
-writer = pd.ExcelWriter(fnDatosPrueba, engine='openpyxl')
-book = load_workbook(fnDatosPrueba)
-writer.book = book
-writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-
-PatrocinadoresDeComisionReglamento.to_excel(writer,sheet_name='Sheet1',startcol=0, startrow=0)
-datoAEscribir.to_excel(writer, startrow=2, startcol=2)
+#PatrocinadoresDeComisionReglamento.to_excel(fnDatosPrueba)
 
 #datoAEscribir.to_excel
 
-writer.save()
 
 reglamentoDF = PatrocinadoresDeComisionReglamento
 
 arrayReglamento = reglamentoDF.to_numpy()
+
+#arrayReglamento[1][1] = 'hola'
+
+#newDataFrame = pd.DataFrame(arrayReglamento, index = reglamentoDF.index, columns=reglamentoDF.columns)
+
+#newDataFrame.to_excel(fnDatosPrueba,sheet_name='Sheet1',startcol=0, startrow=0)
 
 #print(arrayConstituyentes)
 
@@ -55,16 +62,97 @@ def patrocinadoresDe(nombreConstituyente):
     
     return patrocinadores
 
-print(patrocinadoresDe('Rosa Catrileo'))
 
-a = patrocinadoresDe('Rosa Catrileo')
-
-print("el numero total de patrocinadores a Rosa Catrileo es "+ str(len(a)))
+        
 
 
+def aQuienPatrocina(nombreConstituyente):
+    patrocinador = 0
+    patrocinado = 1
+    viejoDF = pd.read_excel(fnDatosPrueba, usecols=[1,2])
+    arrayReglamento = viejoDF.to_numpy()
+
+    for patrocinio in arrayReglamento:
+        if patrocinio[patrocinador] == nombreConstituyente:
+            return patrocinio[patrocinado]
+
+def nombreValidoPatrocinador(nombreConstituyente):
+    patrocinador = 0
+    viejoDF = pd.read_excel(fnDatosPrueba, usecols=[1,2])
+    arrayReglamento = viejoDF.to_numpy()
+
+    var = False
+
+    for patrocinio in arrayReglamento:
+        if patrocinio[patrocinador] == nombreConstituyente:
+            var = True
+    return var
+
+def nombreValidoPatrocinado(nombreConstituyente):
+    patrocinado = 1
+    viejoDF = pd.read_excel(fnDatosPrueba, usecols=[1,2])
+    arrayReglamento = viejoDF.to_numpy()
+
+    var = False
+
+    for patrocinio in arrayReglamento:
+        if patrocinio[patrocinado] == nombreConstituyente:
+            var = True
+    return var
 
 
-#CREATEVIRUS(ACAAAA)
+
+def yaPatrocino(nombreConstituyente):
+
+    if type(aQuienPatrocina(nombreConstituyente))==str:
+        return True
+    else:
+        return False
+
+def IngresarPatrocinio(ConstituyentePatrocinador, ConstituyentePatrocinado):
+
+    if yaPatrocino(ConstituyentePatrocinador):
+        print("Este constituyente ya patrocino a alguien")
+    
+    elif nombreValidoPatrocinador(ConstituyentePatrocinador) == False:
+        print("Este patrocinador no existe")
+
+    elif nombreValidoPatrocinador(ConstituyentePatrocinado) == False:
+        print("Este patrocinado no existe")
+
+    else:
+        viejoDF = pd.read_excel(fnDatosPrueba, usecols=[1,2])
+        viejoArr = viejoDF.to_numpy()
+        patrocinador = 0
+        patrocinado = 1
+
+        for patrocinio in viejoArr:
+            if patrocinio[patrocinador] == ConstituyentePatrocinador:
+                patrocinio[patrocinado] = ConstituyentePatrocinado
+
+        newDataFrame = pd.DataFrame(viejoArr, index = viejoDF.index, columns=viejoDF.columns)
+
+        newDataFrame.to_excel(fnDatosPrueba,sheet_name='Sheet1',startcol=0, startrow=0)
+
+        print("El patrocinio ha sido ingresado con éxito!")
+
+while(True):
+
+    patrocinadorUsuario = input("Ingrese nombre constituyente patrocinador: ")
+
+    patrocinadoUsuario = input("Ingrese nombre constituyente patrocinado: ")
+
+
+    IngresarPatrocinio(patrocinadorUsuario, patrocinadoUsuario)
+
+
+
+
+        
+
+
+
+
 
 
 
